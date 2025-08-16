78f3ba8ba501b9885909954a09be7d653b01f92e
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2025-08-16 20:43:25 +0700

    the odin project calculator solutions

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Store all components on HTML in our JS
+  let clear = document.querySelector("#clear-btn");
+  let equal = document.querySelector(".equal");
+  let decimal = document.querySelector(".decimal");
+
+  let numbers = document.querySelector(".number");
+  let operators = document.querySelector(".operator");
+
+  let previousScreen = document.querySelector(".previous")
+  let currentScreen = document.querySelector(".current")
+})
